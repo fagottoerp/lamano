4aa6defc36eacfba7631e0728869638f62cc2fa0
fagottoerp <fagottoerp@gmail.com>
2026-06-03 09:35:37 +0000

    Register for APNs remote notifications in AppDelegate

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,6 +1,7 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 #import <FirebaseCore/FirebaseCore.h>
+#import <UserNotifications/UserNotifications.h>
 
 @implementation AppDelegate
 
@@ -12,8 +13,28 @@
   if (@available(iOS 10.0, *)) {
     [UNUserNotificationCenter currentNotificationCenter].delegate = (id<UNUserNotificationCenterDelegate>) self;
   }
+
+  // Ask iOS to register for remote notifications and receive an APNs token.
+  [application registerForRemoteNotifications];
+
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  const unsigned char *dataBuffer = (const unsigned char *)[deviceToken bytes];
+  if (!dataBuffer) {
+    return;
+  }
+  NSMutableString *hexToken = [NSMutableString stringWithCapacity:(deviceToken.length * 2)];
+  for (NSUInteger i = 0; i < deviceToken.length; ++i) {
+    [hexToken appendFormat:@"%02x", dataBuffer[i]];
+  }
+  NSLog(@"APNs device token: %@", hexToken);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+  NSLog(@"Failed to register for remote notifications: %@", error);
 }
 
 - (void)didInitializeImplicitFlutterEngine:(NSObject<FlutterImplicitEngineBridge>*)engineBridge {
